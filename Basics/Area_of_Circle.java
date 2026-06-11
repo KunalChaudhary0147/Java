598faca551b0b05a4cde883b0c3c896f97e46018
KunalChaudhary0147 <kunalchaudhary0147@gmail.com>
2026-06-11 12:48:34 +0530

    11-06-2026

--- Basics/Area_of_Circle.java
+++ Basics/Area_of_Circle.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Area_of_Circle {
     public static void main(String[] args)
     {
-        double r=5;
+        // double r=5;
+        // System.out.println("Area of circle is : "+(3.1415*r*r));
 
+
+        // Using input
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nEnter the radius of circle : ");
+        double r=input.nextDouble();
         System.out.println("Area of circle is : "+(3.1415*r*r));
+        input.close();
     }
 }
